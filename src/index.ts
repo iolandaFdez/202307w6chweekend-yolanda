d05af9629d7184847b8c265d698e61472a733969
yolanda fdez <iolandafdez@gmail.com>
2023-08-25 19:36:56 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import { createServer } from 'http';
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+  console.log('Servidor escuchando en http://localhost:3000');
+});
+
+const server = createServer((req, res) => {
+  console.log('Hola mundo');
+  res.write('<h1>Hola Mundo</h1>');
+  res.end();
+});
+
+server.listen(PORT);
+
+server.on('listening', () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+server.on('error', (error) => {
+  console.log(`Error ${error.message}`);
+});
