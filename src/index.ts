17f2966e2dfa0088ac0d5f3063a909cf80bdc1c3
yolanda fdez <iolandafdez@gmail.com>
2023-08-26 14:05:16 +0200

    Change index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import 'dotenv/config';
-import express from 'express';
-import { createServer } from 'http';
 
-const app = express();
+import { createServer } from 'http';
+import { app } from './app.js';
 
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hola Mundo');
-});
 
 app.listen(3000, () => {
   console.log('Servidor escuchando en http://localhost:3000');
